e4efd43819d5f70ecd7efffda27c8d2e43ff10a5
xhrg <634789257@qq.com>
2022-04-20 02:08:37 +0800

    代码移动

--- src/test/java/com/github/xhrg/dlock/test/Test.java
+++ src/test/java/com/github/xhrg/dlock/test/Test.java
@@ -1,10 +1,14 @@
-package com.github.xhrg.dlock;
+package com.github.xhrg.dlock.test;
 
+import com.github.xhrg.dlock.DLock;
+import com.github.xhrg.dlock.DLockException;
+import com.github.xhrg.dlock.MysqlLock;
 import com.mysql.cj.jdbc.MysqlDataSource;
 
-public class Main {
+public class Test {
 
     public static void main(String[] args) throws DLockException {
+
         MysqlDataSource ds = new MysqlDataSource();
         ds.setUrl("jdbc:mysql://127.0.0.1:3306/my_test_db");
         ds.setPassword("123456");
