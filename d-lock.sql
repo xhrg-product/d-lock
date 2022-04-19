a1453bfa30d533a70de62850e40daf6b84a85f03
xhrg <634789257@qq.com>
2022-04-20 00:16:55 +0800

    完善代码

--- d-lock.sql
+++ d-lock.sql
@@ -1,10 +1,24 @@
 -- dlock table
 
+--version 0.2
 CREATE TABLE `d_lock` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
-  `lock_time` varchar(45) DEFAULT NULL,
-  `status` varchar(20) COLLATE utf8mb4_bin DEFAULT NULL commont 'lock, unlock',
+  `id`     int(11)       NOT NULL AUTO_INCREMENT,
+  `name`   varchar(100)  NOT NULL DEFAULT '' COMMENT '锁名称',
+  `status` varchar(20)   NOT NULL DEFAULT '' COMMENT '锁状态,lock和unlock',
+  `time`   varchar(45)   NOT NULL DEFAULT '' COMMENT '锁时间,字符串存储,计算在程序中',
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_name` (`name`) USING HASH
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+
+--version 0.1
+--CREATE TABLE `d_lock` (
+--  `id` int(11) NOT NULL AUTO_INCREMENT,
+--  `name` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+--  `lock_time` varchar(45) DEFAULT NULL,
+--  `status` varchar(20) COLLATE utf8mb4_bin DEFAULT NULL commont 'lock, unlock',
+--  PRIMARY KEY (`id`),
+--  UNIQUE KEY `unique_name` (`name`) USING HASH
+--) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+
--- src/main/java/com/github/xhrg/dlock/SqlCommd.java
+++ src/main/java/com/github/xhrg/dlock/SqlCommd.java
@@ -3,11 +3,11 @@ package com.github.xhrg.dlock;
 public class SqlCommd {
 
     public static String SQL_LOCK =
-        "update d_lock set `status` = 'lock', lock_time = ?  where `name` = ? and ( `status` = 'unlock' or lock_time < ? )";
+        "update d_lock set `status` = 'lock', `time` = ? where `name` = ? and ( `status` = 'unlock' or `time` < ? )";
 
-    public static String SQL_SELECT_NAME = " select `name` from d_lock ";
+    public static String SQL_SELECT_NAME = "select `name` from d_lock";
 
-    public static String SQL_INSERT = "  insert into d_lock ( `name`, `lock_time`, `status` )values(? , ? ,'lock') ";
+    public static String SQL_INSERT = "insert into d_lock ( `name`, `status` , `time`) values ( ? , 'lock',  ?)";
 
-    public static String SQL_UNLOCK = " update d_lock set `status` = 'unlock' where `name` = ? ";
+    public static String SQL_UNLOCK = "update d_lock set `status` = 'unlock' where `name` = ?";
 }
